d01123a3322fd66233588a30d89c8cf17c4db5b8
Jones Kwame Osei <oseikwamejones@gmail.com>
2024-07-04 14:09:59 +0100

    Changed Deployment

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace myWebCVApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
